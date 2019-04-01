9dab16d1c048c1ae15004641ef86329f84667c36
zhivkovvzhivko <zhivkovv.zhivko@gmail.com>
2019-04-01 23:33:59 +0300

    Added MVC structure + classes autoloading WIP

--- CarSpending/mvc/index.php
+++ CarSpending/mvc/index.php
@@ -1,25 +1,26 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+spl_autoload_register();
 date_default_timezone_set('Europe/Sofia');
 
-require_once('csvReader.php');
-require_once('display.php');
-require_once('translations.php');
+use Controller\CsvReader;
+use View\Display;
+use Translations\Translations;
 
-$reader = new CsvReader();
+$csvReader = new CsvReader();	
 $display = new Display();
 $translations = new Translations();
 
-$refuelData = $reader->readCsv('refuelData.csv');
-$spendingsData = $reader->calcSpendings($refuelData);
-$summaryData = $reader->calcSummary($refuelData);
+$refuelData = $csvReader->readCsv('refuelData.csv');
+$spendingsData = $csvReader->calcSpendings($refuelData);
+$summaryData = $csvReader->calcSummary($refuelData);
 
 $outputData = [];
 foreach(['bg', 'en'] as $lang) {
-	$outputData[$lang] = $display->prepareHTMLOutput($spendingsData, $summaryData, $translations::translations[$lang]);
+	$outputData[$lang] = $display->prepareHTMLOutput($spendingsData, $summaryData, $translations->translations[$lang]);
 }
 
-// saveToFile('en.html', $outputData['en']);
 $display->displayData($outputData['bg']);
 $display->displayData($outputData['en']);
+$csvReader->saveToFile('en.html', $outputData['en']);
